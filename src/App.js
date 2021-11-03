87ff0c4886bfb65f83c9da3fe0cb8d5e5fe3fc9e
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2021-11-03 15:50:03 +0000

    examples for hooks lecture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import './App.css';
 import Checkbox from './Checkbox'
 import LessText from './LessText'
 import InputPhrase from './InputPhrase'
 import InputPhraseND from './InputPhraseNoDependency'
 import Example from './count'
+import Counter from './reducerex'
+import ShoppingList from './shoppinglist'
+import CustomTextInput from './simpleRef';
 
 function App() {
   return (
     <div className="App">
-           <Checkbox /><br/>
-           <Example />
-           <LessText
+          <div className="component"><Checkbox /> </div> 
+          <div className="component"><Example /> </div> 
+          <div className="component"><LessText
               text={`Focused, hard work is the real key
                 to success. Keep your eyes on the goal, 
                 and just keep taking the next step 
                 towards completing it.`}
               maxLength={35}  />
-               <br/>
-          <InputPhrase />
-          <InputPhraseND />
- 
+              </div> 
+          <div className="component"><InputPhrase /></div> 
+          <div className="component"><InputPhraseND /></div> 
+          <div className="component"><Counter /></div> 
+          <div className="component"><ShoppingList /></div> 
+          <div className="component"><CustomTextInput /></div> 
     </div>
   );
 }
