c8b2ea72fa2d466398258dc3ea82c445a168c25e
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-10-17 12:32:53 +0100

    update week 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
-import BasicStateEx from "./basicStateExample";
-import LessText from "./Lesstext";
-import Numbers from "./Numbers";
-import CountButtonA from "./CountButtonA";
-import CountButtonB from "./CountButtonB";
-import StarRating from "./Stars";
-import InputPhrase from "./InputPhrase";
-import Simple from "./Simple";
+import CountButtonA from "./components/CountButtonA";
+import CountButtonB from "./components/CountButtonB";
+import StarRating from "./components/Stars";
+import InputPhrase from "./components/InputPhrase";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,23 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <BasicStateEx />
-      <br></br>
-      <LessText
-        text={`Burning household rubbish in giant incinerators to make electricity is
-        now the dirtiest way the UK generates power, BBC analysis has found.
-        Nearly half of the rubbish produced in UK homes, including increasing
-        amounts of plastic, is now being incinerated. Scientists warn it is a
-        “disaster for the climate” - and some are calling for a ban on new
-        incinerators. The BBC examined five years of data from across the
-        country, and found that burning waste produces the same amount of
-        greenhouse gases for each unit of energy as coal power, which was
-        abandoned by the UK last month.`}
-        maxLength={150}
-      />
-       <br></br>
-      <Numbers />
-      {/*<div>
+       <div>
         <CountButtonA count={count} onClick={() => setCount(count + 1)} />
         <CountButtonA count={count} onClick={() => setCount(count + 1)} />
       </div>
@@ -45,8 +25,7 @@ function App() {
         <StarRating />
       </div>
       <InputPhrase /> 
-      <Simple />
-          */}
+         
     </div>
   );
 }
--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import Star from "./Star";
-import { createArray } from "./lib";
 
 export default function StarRating({ totalStars = 5 }) {
-  
+  const createArray = length => [...Array(length)];
   const [selectedStars, setSelectedStars] = useState(3);
 
   return (
